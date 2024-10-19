fd74db64ec30ceda7385159c6e0b5f4d9e7aacbf
Andrey <aazhiyanov@gmail.com>
2024-10-19 08:38:17 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZhiyanovPlugin
+{
+    public class Class1 : IPluggable
+    {
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
